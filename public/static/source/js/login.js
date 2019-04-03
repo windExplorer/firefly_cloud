fe2dec34a3bb6f50ad07db967b0aa57ce7aa29ea
sshift <1479221500@qq.com>
2019-04-03 08:14:13 +0800

    renew

--- public/static/source/js/login.js
+++ public/static/source/js/login.js
@@ -12,15 +12,25 @@ layui.use(['layer', 'form', 'jquery'], () => {
       return
     $('input[name=password]').val('')
     ele.addClass(ele.attr('data-anim'))
+    let load
     $.ajax({
       url: ele.attr('fiy-link'),
       data: data.field,
       type: 'post',
+      beforeSend: function() {
+        load = layer.msg('正在登录...', {
+          time: 0
+        })
+      },
       success: (res) => {
+        layer.close(load)
         if(res && res.data){
-          location.href = ele.attr('fiy-to')
+          layer.msg('登录成功，正在跳转...')
+          setTimeout(function() {
+            location.href = ele.attr('fiy-to')
+          }, 1000)
         } else {
-          return
+          layer.msg('登录失败，请检查用户名密码')
         }
       },
       complete: () => {
