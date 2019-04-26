f2001e67019b6f209e2bb840d7497e26c1ad05eb
sshift <1479221500@qq.com>
2019-04-26 08:33:52 +0800

    更新

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -234,19 +234,29 @@
             $this->User = $check;
             $flag = $this->Update($table, $check['id'], ['password' => $pwd, 'salt' => $salt, 'vcode' => '', 'uptime' => time()]);
             if($flag){
-                $this->Addlog($table, '('.$res['username'].')修改密码成功', 2);
-                return $this->Restful(true, 1, '修改密码成功!');
+                $this->Addlog($table, '('.$res['username'].')找回密码成功', 2);
+                return $this->Restful(true, 1, '找回密码成功!');
             }else{
-                return $this->Restful(false, 0, '修改密码失败, 请与站长联系!');
+                return $this->Restful(false, 0, '找回密码失败, 请与站长联系!');
             }
 
         }
 
-        /* 获取验证码 */
+        /**
+         *  获取验证码 
+         *  email: 邮箱
+         *  type: 1.找回密码  2.修改邮箱
+         * 
+         * */
         public function getvcode()
         {
             $table = 'user';
             $res = input('post.');
+            switch($res['type']){
+                case 1: $subject_text = '找回密码'; $content_href = request()->header()['origin'].'/resetpwd'; $content_href_text = '重置密码链接'; $logtext = '找回密码'; break;
+                case 2: $subject_text = '修改邮箱'; $content_href = 'javascript:;'; $content_href_text = '修改邮箱'; $logtext = '修改邮箱'; break;
+                default: $subject_text = '其他'; $content_href = 'javascript:;'; $content_href_text = '其他'; break;
+            }
             if(!empty($res['email'])){
                 if(!filter_var($res['email'], FILTER_VALIDATE_EMAIL)){
                     return $this->Restful(false, 0, '不是一个合法的邮箱地址!');
@@ -258,12 +268,13 @@
             }else{
                 return $this->Restful(false, 0, '请输入邮箱!');
             }
+
             $res = $check;
             $this->User = $check;
 
             # 发送邮件
             $vcode = getLenRand(6);
-            $subject = '【'.sysConf('website_group_name').'】找回密码!';;
+            $subject = '【'.sysConf('website_group_name').'】'.$subject_text.'!';;
             $content = '<style>
             .content {
                 width: 500px;
@@ -310,14 +321,14 @@
         </style>
         <div class="content">
             <div class="box">
-                <p><a href="'.request()->header()['origin'].'/resetpwd" target="_blank" class="important important-1">重置密码链接</a></p>
+                <p><a href="'.$content_href.'" target="_blank" class="important important-1">'.$content_href_text.'</a></p>
                 <p> (该验证码为一次性，无时间限制，但重复获取将被替代)!</p>
                 <p>亲爱的 <i class="important">'.$check['username'].'</i> </p>
                 <p>验证码为: <i class="important">'.$vcode.'</i> </p>
             </div>
         </div>';
             $context = '用户名：'.$check['username']. '   验证码：'.$vcode.'   (验证码一次性且长期有效，但重复获取将被替代!)';
-            $send_flag = $this->sendMail($res['email'], $subject, $content, $check['username'], $context, $text = '发送找回密码验证码');
+            $send_flag = $this->sendMail($res['email'], $subject, $content, $check['username'], $context, '发送'.$logtext.'验证码');
             if(!$send_flag){
                 return $this->Restful(false, 0, '验证码发送失败，请联系站长！');
             }
@@ -326,6 +337,86 @@
             $this->Update($table, $check['id'], ['vcode' => $vcode, 'uptime' => time()]);
 
             return $this->Restful(true, 1, '验证码已发送至您的邮箱，请注意查收，也请注意查看垃圾箱!');
+
+        }
+
+        /* 修改密码 */
+        public function changepwd()
+        {
+            $table = 'user';
+            $res = input('post.');
+            $res = $this->removeXSS($res);
+            foreach($res as $k => $v){
+                $res[$k] = str_replace(' ','',$res[$k]);
+            }
+            $user = $this->checkToken();
+            if(empty($user)){
+                return $this->Restful(false, 0, '令牌错误，请重新登录');
+            }
+            if(sha1($res['password1'].$user['salt']) !== $user['password']){
+                return $this->Restful(false, 0, '原密码不正确');                
+            }
+            if($res['password2'] !== $res['password3']){
+                return $this->Restful(false, 0, '两次新密码不一致');
+            }
+            if(mb_strlen($res['password2']) < 5 || mb_strlen($res['password2']) > 16){
+                return $this->Restful(false, 0, '密码字符数在5-16之间,且不能有空格!');
+            }
+            #
+            $salt = getLenRand(5);
+            $pwd = sha1($res['password2'].$salt);
+            $flag = $this->Update($table, $user['id'], ['password' => $pwd, 'salt' => $salt, 'uptime' => time()]);
+            if($flag){
+                $this->Addlog($table, '('.$user['username'].')修改密码成功', 2);
+                return $this->Restful(true, 1, '修改密码成功!');
+            }else{
+                return $this->Restful(false, 0, '修改密码失败, 请与站长联系!');
+            }
+        }
+
+        /* 修改邮箱 */
+        public function changeemail()
+        {
+            $table = 'user';
+            $res = input('post.');
+            $res = $this->removeXSS($res);
+            foreach($res as $k => $v){
+                $res[$k] = str_replace(' ','',$res[$k]);
+            }
+            $user = $this->checkToken();
+
+            if(!empty($res['vcode'])){
+                if($res['vcode'] !== $user['vcode']){
+                    return $this->Restful(false, 0, '验证码错误!');
+                }
+            }else{
+                return $this->Restful(false, 0, '请输入验证码!');
+            }
+
+            if($res['email'] == $user['email']){
+                return $this->Restful(false, 0, '邮箱没有变化!');
+            }
+            if($res['email'] !== $res['email2']){
+                return $this->Restful(false, 0, '两次新邮箱不一致!');
+            }
+            if(!empty($res['email'])){
+                $check = $this->Retrieve($table, ['email' => $res['email']]);
+                if(!empty($check)){
+                    return $this->Restful(false, 0, '邮箱已存在!');
+                }
+                if(!filter_var($res['email'], FILTER_VALIDATE_EMAIL)){
+                    return $this->Restful(false, 0, '不是一个合法的邮箱地址!');
+                }
+            }else{
+                return $this->Restful(false, 0, '请输入邮箱!');
+            }
+            $flag = $this->Update($table, $user['id'], ['email' => $res['email'], 'vcode' => '', 'uptime' => time()]);
+            if($flag){
+                $this->Addlog($table, '('.$user['username'].')修改邮箱成功', 2);
+                return $this->Restful(true, 1, '修改邮箱成功!');
+            }else{
+                return $this->Restful(false, 0, '修改邮箱失败, 请与站长联系!');
+            }
 
         }
 
